a964140aa991382f725e2f9455cf7ccc9156a3d6
bamberjo <32629239+bamberjo@users.noreply.github.com>
2018-11-09 19:55:36 -0800

    Untested Servo Parsing Code

--- OnboardServoControl.ino
+++ OnboardServoControl.ino
@@ -50,24 +50,21 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 
 //Servo Settings
-const int SERVOFREQ 50  // This gives a signal every 20ms
-const int SERVOMIN  205 // this is the 'minimum' pulse length count (out of 4096) approx 1ms
-const int SERVOMAX  410 // this is the 'maximum' pulse length count (out of 4096) approx 2ms
+const int SERVOFREQ =  60;  // This gives a signal every 20ms
+const int SERVOMIN = 150;//205 // this is the 'minimum' pulse length count (out of 4096) approx 1ms
+const int SERVOMAX = 600;//410 // this is the 'maximum' pulse length count (out of 4096) approx 2ms
 const int nServos = 8;
-const int fAngle = 0;
-const int bAngle = 180;
+const int fAngle[8] = {0,0,0,0,0,0,0,0};
+const int bAngle[8] = {180,180,180,180,180,180,180,180};
 
 //Input Channels
-const int POT1 = 3;
-const int POT2 = 4;
-const int SWITCH = 5;
+const int POT1 = 2;
+const int POT2 = 3;
+const int SWITCH = 6;
 
 //Input Settings
-const int LOWTHRESH = 15;
-const int HIGHTHRESH = 85;
-
-unsigned long lowMicroseconds = 2000*LOWTHRESH/100;
-unsigned long highMicroseconds = 2000*HIGHTHRESH/100;
+unsigned long lowMicroseconds = 1200;
+unsigned long highMicroseconds = 1800;
 
 //Variables for which is the current selected store
 int row = 0;
@@ -78,13 +75,16 @@ unsigned long pulselength;
 
 int angle[nServos];
 
-void setServoAngle(servoPin,angle){
+void setServoAngle(int servoPin,float angle){
   pwm.setPWM(servoPin,0,round(map(angle,0,180,205,410)));
 }
 
-int parseInput(pin,lowThresh,highThresh){
+int parseInput(int pin,int lowThresh,int highThresh){
   //This will parse out the inputs
   pulselength = pulseIn(pin,HIGH);
+  //Serial.println(pulselength);
+  //Serial.println(lowThresh);
+  //Serial.println(highThresh);
 
   if(pulselength < lowThresh){
     return 0;
@@ -100,16 +100,25 @@ uint8_t servonum = 0;
 
 void setup() {
 
+  Serial.begin(57600);
+
   pwm.begin();
+  pinMode(POT1, INPUT);
+  pinMode(POT2, INPUT);
+  pinMode(SWITCH, INPUT);
   
   pwm.setPWMFreq(SERVOFREQ);  // Analog servos run at ~60 Hz updates
 }
 
 void loop() {
+  delay(100);
   // Check for which pair pots say it is in
   col = parseInput(POT1,lowMicroseconds,highMicroseconds);
   row = parseInput(POT2,lowMicroseconds,highMicroseconds);
   switchPos = parseInput(SWITCH,lowMicroseconds,highMicroseconds);
+  //Serial.println(col);
+  //Serial.println(row);
+  //Serial.println(switchPos);
 
   //Set all angles to their default
   for(int i = 0; i < nServos; i++){
@@ -124,14 +133,17 @@ void loop() {
           switch(switchPos){
             case 0:
               //A1L
-              angle[0] = fAngle;
+              angle[0] = fAngle[0];
+              Serial.println("A1L");
               break;
             case 1:
               //Nothing
+              Serial.println("None");
               break;
-            case 2;
+            case 2:
               //A1R
-              angle[1] = fAngle;
+              Serial.println("A1R");
+              angle[1] = fAngle[1];
               break;
           }
           break;
@@ -139,14 +151,17 @@ void loop() {
           switch(switchPos){
             case 0:
               //A2L
-              angle[2] = fAngle;
+              Serial.println("A2L");
+              angle[2] = fAngle[2];
               break;
             case 1:
               //Nothing
+              Serial.println("None");
               break;
-            case 2;
+            case 2:
               //A2R
-              angle[3] = fAngle;
+              Serial.println("A2R");
+              angle[3] = fAngle[3];
               break;
           }
           break;
@@ -154,14 +169,17 @@ void loop() {
           switch(switchPos){
             case 0:
               //A3L
-              angle[4] = fAngle;
+              Serial.println("A3L");
+              angle[4] = fAngle[4];
               break;
             case 1:
               //Nothing
+              Serial.println("None");
               break;
-            case 2;
+            case 2:
               //A3R
-              angle[5] = fAngle;
+              Serial.println("A3R");
+              angle[5] = fAngle[5];
               break;
           }
           break;
@@ -173,14 +191,17 @@ void loop() {
           switch(switchPos){
             case 0:
               //B1L
-              angle[0] = bAngle;
+              Serial.println("B1L");
+              angle[0] = bAngle[0];
               break;
             case 1:
               //Nothing
+              Serial.println("None");
               break;
-            case 2;
+            case 2:
               //B1R
-              angle[1] = bAngle;
+              Serial.println("B1R");
+              angle[1] = bAngle[1];
               break;
           }
           break;
@@ -188,14 +209,17 @@ void loop() {
           switch(switchPos){
             case 0:
               //B2L
-              angle[6] = fAngle;
+              Serial.println("B2L");
+              angle[6] = fAngle[6];
               break;
             case 1:
               //Nothing
+              Serial.println("None");
               break;
-            case 2;
+            case 2:
               //B2R
-              angle[7] = fAngle;
+              Serial.println("B2R");
+              angle[7] = fAngle[7];
               break;
           }
           break;
@@ -203,14 +227,17 @@ void loop() {
           switch(switchPos){
             case 0:
               //B3L
-              angle[4] = bAngle;
+              Serial.println("B3L");
+              angle[4] = bAngle[4];
               break;
             case 1:
               //Nothing
+              Serial.println("None");
               break;
-            case 2;
+            case 2:
               //B3R
-              angle[5] = bAngle;
+              Serial.println("B3R");
+              angle[5] = bAngle[5];
               break;
           }
           break;
@@ -219,24 +246,28 @@ void loop() {
     case 2: 
       switch(col){
         case 0:
-          
+          Serial.println("None");
           break;
         case 1:
           switch(switchPos){
             case 0:
-              //B2L
-              angle[6] = bAngle;
+              //C2L
+              Serial.println("C2L");
+              angle[6] = bAngle[6];
               break;
             case 1:
               //Nothing
+              Serial.println("None");
               break;
-            case 2;
-              //B2R
-              angle[7] = bAngle;
+            case 2:
+              //C2R
+              Serial.println("C2R");
+              angle[7] = bAngle[7];
               break;
           }
           break;
         case 2: 
+          Serial.println("None");
           
           break;
       }
